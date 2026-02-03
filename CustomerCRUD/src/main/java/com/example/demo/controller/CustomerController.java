f7a4668b95e64ff5ffd391e28b4c3121129340a7
vijay <vijayrajdhembare8430@gmail.com>
2026-02-03 12:39:58 +0530

    new message

--- CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CustomerController {
 	@Autowired
 	private CustomerService cs;
